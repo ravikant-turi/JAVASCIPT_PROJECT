043b5b01f1d8d6ab41f6e32f919d0264016fbc95
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2024-04-02 19:57:32 +0000

    learn about the dataType in javascript

---
+++ 01_BASIC/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict" ;//treat all js code as newer version
+// alert(3+3) : we are using node js not browser
+
+console.log(3+3);
+
+console.log("Ravikant turi");
+
+let name="ravi"
+
+
+//type :
+//1.number=> 2 to the power 53
+//2.bigint
+//3.string=""
+//4.boolean=>true /false
+//5.null =>standar value
+//6. undefined
+//7. symbol=> unique
+
+//8. Object
+
+console.log(typeof(undefined))
