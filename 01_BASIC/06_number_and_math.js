a85287ba92b078784423a03537b4a647b06fd90a
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2024-04-04 16:25:44 +0000

    NUMBER AND MATH

---
+++ 01_BASIC/06_number_and_math.js
@@ -0,0 +1,41 @@
+// console.log("let's  learn about NUMBER and Math")
+
+// const score=324
+// console.log(score);
+
+
+// const balance=new Number(2334);
+// console.log(balance);//[Number: 2334]
+
+
+// console.log(balance.toString().length);//4
+// console.log(balance.toFixed(2));//2334.00
+
+// const otherNumber=4334.53432;
+// console.log(otherNumber.toPrecision(6));//4334.53
+
+// const hundereds=10000000000;
+// console.log(hundereds.toLocaleString());
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+// ======================================MATH===============================
+
+// console.log(Math);// object [Math] {}
+// console.log(Math.abs(-47839));//47839
+// console.log(Math.round(4.3));//4
+// console.log(Math.ceil(4.2));//upper value 5
+// console.log(Math.floor(4.2));//lower value 4
+
+// console.log(Math.random()); //rang between (0-1)
+
+console.log(Math.round(Math.random()*10)+1);
+console.log(Math.round(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
