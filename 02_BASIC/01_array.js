790f8f01f12dfca565b12cb33b63755cbc6d6eb3
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2024-04-07 22:28:21 +0000

    array

---
+++ 02_BASIC/01_array.js
@@ -0,0 +1,13 @@
+console.log("========WELCOME TO ARRAY ===========");
+
+const newArray=[0,1,2,3,4];
+for(let i=0;i<newArray.length;i++){
+    console.log(newArray[i]);
+}
+// newArray.push(6);
+// console.log(newArray);
+// newArray.pop();
+// console.log(newArray);
+
+
+
